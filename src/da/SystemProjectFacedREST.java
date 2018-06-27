32567f44262d963e3b05ebbbf2fcbc0fe3ab855f
saharf <sahar.fares89@gmail.com>
2018-06-27 19:44:34 +0300

    ss

---
+++ src/da/SystemProjectFacedREST.java
@@ -0,0 +1,79 @@
+package da; 
+
+import java.util.List;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import model.SystemProject;
+
+@javax.inject.Singleton
+@Path("systemProject")
+@Produces({ "application/json" })
+public class SystemProjectFacedREST extends AbstractFacade<SystemProject> {
+	private EntityManager em;
+	public SystemProjectFacedREST() {
+		super(SystemProject.class);
+	}	
+	@PUT
+	@Override
+	@Consumes({ "application/xml", "application/json" })
+	public Response edit(SystemProject entity) {
+		return super.edit(entity);
+	}
+	@DELETE
+	@Path("remove/{id}")
+	public Response remove(@PathParam("id") Integer id) {
+		return super.remove(super.find(id));
+	}
+	@GET
+	@Path("{id}")
+	@Produces({ "application/json" })
+	public SystemProject find(@PathParam("id") Integer id) {
+		return super.find(id);
+	}
+	
+
+	@GET
+	@Override
+	@Produces({ "application/json"})
+	public List<SystemProject> findAll() {
+		return super.findAll();
+	}
+
+	@GET
+	@Path("{from}/{to}")
+	@Produces({ "application/json" })
+	public List<SystemProject> findRange(@PathParam("from") Integer from,
+			@PathParam("to") Integer to) {
+		return super.findRange(new int[] { from, to });
+	}
+
+	@GET
+	@Path("count")
+	@Produces("text/plain")
+	public String countREST() {
+		return String.valueOf(super.count());
+	}
+
+	@Override
+	protected EntityManager getEntityManager() {
+		em = EntityManagerHelper.getEntityManager();
+		return em;
+	}
+	
+}
+
